8105b89371ee52f9f5d7d34a90f2dc972b060a4e
Tvisha2008 <75105270+Tvisha2008@users.noreply.github.com>
2021-01-14 21:33:23 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var boxbase, boxleft, boxright;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+
+	//create red box
+	 boxbase = Bodies.rectangle(width/2, 645, 200, 20 , {isStatic:true} );
+	 World.add(world, boxbase);
+
+	 boxleft = Bodies.rectangle(width/2-100, 600, 20, 100 , {isStatic:true} );
+	 World.add(world, boxleft);
+
+	 boxright = Bodies.rectangle(width/2+100, 600, 20, 100 , {isStatic:true} );
+	 World.add(world, boxright);
+	 
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+  rectMode(CENTER);
+  fill ("red");
+  rect(boxbase.position.x,boxbase.position.y,200,20);
+  rect(boxleft.position.x,boxleft.position.y,20,100);
+  rect(boxright.position.x,boxright.position.y,20,100);
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	Matter.Body.setStatic(packageBody, false);
+    
+  }
+}
+
+
+
